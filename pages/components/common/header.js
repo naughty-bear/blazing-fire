5e37dd0e13fe89580d16782a2e6405102831c59d
naughty-bear <Song_xl1123@126.com>
2023-04-03 22:17:03 +0800

    01-header

--- pages/components/common/header.js
+++ pages/components/common/header.js
@@ -1,11 +1,7 @@
 import React from 'react'
 export default function header() {
     return (
-<<<<<<< HEAD
-        <header className="navbar py-6 px-10">
-=======
         <header className="navbar py-5  bg-black max-w-screen-xl mx-auto">
->>>>>>> 1fd68f0698d3555b342248312b1b399d6e60b61c
             <div className="navbar-start">
                 <div className="dropdown">
                     <label tabIndex={0} className="btn btn-ghost lg:hidden">
@@ -19,11 +15,7 @@ export default function header() {
                         <li><a>Contact</a></li>
                     </ul>
                 </div>
-<<<<<<< HEAD
-                <a className="btn bg-transparent w-40 h-10 border-transparent"><img src='/images/logo.png' alt='logo'/></a>
-=======
                 <a className="bg-transparent w-40 h-10 border-transparent bttn"><img src='/images/logo.png' alt='logo'/></a>
->>>>>>> 1fd68f0698d3555b342248312b1b399d6e60b61c
             </div>
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal px-1 text-gray-500">
@@ -35,11 +27,7 @@ export default function header() {
                 </ul>
             </div>
             <div className="navbar-end">
-<<<<<<< HEAD
-                <a className="btn normal-case border-transparent w-44 rounded-full bg-gradient-to-r from-yellow-400 to-red-500 text-white hover:text-black ... " >Back a demo</a>
-=======
                 <a className="normal-case border-transparent w-auto h-auto py-4 px-8 rounded-full bg-gradient-to-r from-yellow-400 to-red-500 text-white hover:text-black bttn" >Back a demo</a>
->>>>>>> 1fd68f0698d3555b342248312b1b399d6e60b61c
             </div>
         </header>
     )
