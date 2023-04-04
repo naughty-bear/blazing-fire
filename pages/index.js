c7f132c0ee0b63dbb925626513e52f1c1cfa9094
naughty-bear <Song_xl1123@126.com>
2023-04-04 20:54:06 +0800

    05-layoutAdjustment

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,8 @@
 import Head from 'next/head';
 import Hero from './components/common/hero';
 import Header from './components/common/header';
+import ProductSections from './components/sections/product-sections'
 import Footer from './components/common/footer';
-import Card from './components/sections/card'
-import Enjoy from './components/sections/enjoy'
-import Vrtechnique from './components/sections/vRtechnique'
-import Virtualtour from './components/sections/virtual-tour'
-import Createworld from './components/sections/createWorld'
-import Choose from './components/sections/choose'
-import Can from './components/sections/can'
-
 export default function Home () {
   return (
     <div className='bg-black'>
@@ -19,13 +12,7 @@ export default function Home () {
       </Head>
       <Header />
       <Hero />
-      <Card/>
-      <Vrtechnique/>
-      <Enjoy/>
-      <Virtualtour/>
-      <Createworld/>
-      <Choose/>
-      <Can/>
+      <ProductSections/>
       <Footer />
     </div>
   );
