67cb1424cee601ad68e7002f3ead92d76c6a0d24
naughty-bear <Song_xl1123@126.com>
2023-04-07 17:05:43 +0800

    09-响应式布局

--- pages/components/sections/createWorld.js
+++ pages/components/sections/createWorld.js
@@ -3,21 +3,21 @@ import React from 'react'
 export default function createWorld() {
     return (
         <section className='bg-gray-900 relative'> 
-            <img src='/images/background/ormanents02.png' className='absolute -z-0 lg:w-2/5 left-10'></img>
+            <img src='/images/background/ormanents02.png' className='absolute z-0 lg:w-2/5 left-10'></img>
             <div className='flex flex-row max-w-screen-xl mx-auto py-32 relative z-10 px-4 Flex'>
                 {/* img区域 */}
-                <div className='md:w-1/2 relative'>
-                    <img src='/images/banner-4.png' className='max-w-xl relative top-10 left-20 z-10'></img>
-                    <img src='/images/banner-2.png' className='w-96 absolute left-36 top-0'></img>
-                    <img src='/images/banner-3.png' className='w-80 rounded-xl absolute right-0 bottom-20'></img>
-                    <img src='/images/banner-3.png' className='w-80 rounded-xl absolute left-0 top-12'></img>
+                <div className='md:w-1/2 relative Margin-B'>
+                    <img src='/images/banner-4.png' className='img relative top-10 left-20 z-10'></img>
+                    <img src='/images/banner-2.png' className='img1 absolute left-20 top-0'></img>
+                    <img src='/images/banner-3.png' className='img2 rounded-xl absolute right-0 bottom-20'></img>
+                    <img src='/images/banner-3.png' className='img2 rounded-xl absolute left-0 top-12'></img>
                     <img src='/images/elements01.png' className='w-20 rounded-xl absolute right-20 top-36'></img>
                     <img src='/images/elements02.png' className='w-36 rounded-xl absolute right-40 top-0'></img>
                     <img src='/images/elements03.png' className='w-20 rounded-xl absolute left-32 bottom-8 z-20'></img>
                     <img src='/images/icon/视频播放.png' className='absolute inset-2/4 z-20 '></img>
                 </div>
                 {/* 文本区域 */}
-                <div className='md:w-1/2 relative Margin-t'>
+                <div className='md:w-1/2 relative Margin-b'>
                     <h2 className='text-white text-5xl font-bold leading-normal ml-10 absolute top-1/3 '> The world your imagination invites you to create is almost limitless</h2>
                 </div>
             </div>
--- pages/components/sections/vRtechnique.js
+++ pages/components/sections/vRtechnique.js
@@ -4,17 +4,17 @@ export default function vRtechnique() {
     return (
         <section className='w-screen bg-black relative'>
         <img src='/images/background/ormanents02.png' className='absolute z-0 w-2/4 -top-32'></img>
-            <div className='max-w-screen-xl mx-auto flex flex-row py-20 px-4 relative z-10 Flex '>
+            <div className='max-w-screen-xl mx-auto flex flex-row py-20 px-4  z-10 Flex '>
                 {/* 左侧 */}
-                <div className='lg:w-3/5 md:w-2/4 flex flex-row flex-wrap md:relative mt-20 '>
-                    <img src='/images/banner-4.png'  className='md:w-96 h-80 absolute top-6 mr-8 Width'></img>
+                <div className='lg:w-3/5 md:w-2/4 flex flex-row flex-wrap relative mt-20 '>
+                    <img src='/images/banner-4.png'  className='md:w-96 h-80 md:absolute top-6 mr-8 Width'></img>
                     <img src='/images/banner-5.jpeg' className='w-96 h-60 absolute top-8 right-0 Display'></img>
-                    <img src='/images/banner-6.jpeg' className='md:w-96 h-60 absolute top-96 mr-10 Width'></img>
+                    <img src='/images/banner-6.jpeg' className='md:w-96 h-60 md:absolute top-96 mr-10 Width'></img>
                     <img src='/images/banner-7.jpeg' className='w-80 h-54 absolute top-80 right-0 Display'></img>
-                    <img src='/images/icon/视频播放.png' className='w-20 md:absolute md:top-72 md:right-96 bttn mx-auto'></img>
+                    <img src='/images/icon/视频播放.png' className='w-20 absolute top-1/2 left-1/3 md:top-1/3 bttn  z-10'></img>
                 </div>
                 {/* 右侧 */}
-                <div className='lg:w-2/5 md:w-2/4 pl-4'>
+                <div className='lg:w-2/5 md:w-2/4 pl-4 Margin-t '>
                     <h2 className='font-bold text-4xl text-white'>Empowering VR to enhance people's lives<span className='span'>.</span></h2>
                     <p className='mt-6 text-gray-400'>Revolutionize Your Real Estate Sales with Interactive Virtual Tours. Studies show that listings with virtual tours receive 87% more views. Provide potential buyers with an engaging and immersive experience of your properties while saving time and resources. Let us show you how to use cutting-edge virtual tour technology to attract better-qualified leads and close more deals. Embrace the change and see the results for yourself.</p>
                     <h3 className='text-white mt-6 text-xl'>This will help you to quickly identify the best solution for your project：</h3>
