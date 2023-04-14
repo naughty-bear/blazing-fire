10f4f82460a36726551123b1c831de28f9e428d2
naughty-bear <Song_xl1123@126.com>
2023-04-15 00:27:33 +0800

    修改网页名

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
     <div className='overflow-hidden'>
       <ThemeProvider theme={theme}>
         <Head>
-          <title>Create Next App</title>
+          <title>BlazingFire</title>
         </Head>
         <Header />
         <Hero />
