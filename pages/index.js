72078055da8fe2db530f46e1dc49644133930567
naughty-bear <Song_xl1123@126.com>
2023-04-15 00:15:49 +0800

    更改文件目录

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
 import Head from 'next/head';
-import Hero from './components/common/hero';
-import Header from './components/common/header';
-import ProductSections from './components/sections/product-sections'
-import Footer from './components/common/footer';
+import Hero from '/components/common/hero';
+import Header from '/components/common/header';
+import ProductSections from '/components/sections/product-sections'
+import Footer from '/components/common/footer';
 import { ThemeProvider } from 'theme-ui';
-import theme from './components/index';
+import theme from '/components/index';
 export default function Home() {
   return (
     <div className='overflow-hidden'>
